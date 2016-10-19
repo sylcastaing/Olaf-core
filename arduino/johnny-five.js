7ec49f9e25bbb99c8a9e7e681d48fabb61ff51ef
sylcastaing <castaing.sylvain@gmail.com>
2016-10-19 20:31:14 +0200

    Add johny-five for weather

--- arduino/johnny-five.js
+++ arduino/johnny-five.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import five from 'johnny-five';
+import Weather from '../api/weather/weather.model';
 
 /**
  * Init function for arduino Board
@@ -10,6 +11,60 @@ import five from 'johnny-five';
 export function init() {
   // Initialize Board
   var Board = new five.Board();
+
+  Board.on('ready', function() {
+
+     // Indoor Temperature
+    new five.Thermometer({
+      pin: 'A0',
+      freq: 1000,
+      toCelsius: function (raw) {
+        var CELSIUS_TO_KELVIN = 273.15;
+        var adcres = 1023;
+        var beta = 3975;
+        var rb = 10000;
+        var tempr = 298.15;
+
+        var rthermistor = (adcres - raw) * rb / raw;
+        var tempc = 1 / (Math.log(rthermistor / rb) / beta + 1 / tempr) - CELSIUS_TO_KELVIN;
+
+        return tempc;
+      }
+    }).on('data', function () {
+      saveWeather('indoorTemp', round(this.celsius));
+    });
+
+    // Outdoor Temperature
+    new five.Thermometer({
+      controller: 'DS18B20',
+      pin: 2,
+      freq: 1000
+    }).on('data', function () {
+      saveWeather('outdoorTemp', round(this.celsius));
+    });
+
+    // Pressure
+    new five.Barometer({
+      controller: 'BMP085',
+      freq: 1000
+    }).on('data', function () {
+      saveWeather('pressure', round(this.pressure * 10));
+    });
+  });
+}
+
+/**
+ * Save Weather object in database
+ * 
+ * @param {any} type
+ * @param {any} value
+ */
+function saveWeather(type, value) {
+  new Weather({
+    date: new Date(),
+    type: type,
+    value: value
+  }).save();
 }
 
 /**
@@ -19,7 +74,7 @@ export function init() {
  * @returns int round value
  */
 function round(value) {
-  result = parsInt(value);
-  return (!isNan(result) ? Math.round(value * 10) / 10 : value);
+  var result = parseInt(value);
+  return (!isNaN(result) ? Math.round(value * 10) / 10 : value);
 }
 
